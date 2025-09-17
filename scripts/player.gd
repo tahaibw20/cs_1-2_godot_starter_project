52cc129f0b0e3963b2b3f13f87f8cdbe28fd28a4
tahaibw20 <talsaood1964@student.pps.net>
2025-09-17 09:58:15 -0700

    added spikes, health, and collecting coins

--- scripts/player.gd
+++ scripts/player.gd
@@ -73,14 +73,24 @@ func update_animation():
 
 
 # TODO: Create health change function for interactions
-func change_health(amount):
+var health = 100
+var maxHealth = 100
+func change_health(amount:int):
+	health+=amount
+	if health < 1:
+		die()
+	if health > maxHealth:
+		health = maxHealth
 	# TODO: Add amount to health (positive = heal, negative = damage)
 	# TODO: Make sure health stays between 0 and maxHealth
 	# TODO: Print the new health value
-	# TODO: Check if health <= 0 for death (optional challenge)
+	# TODO: Check if health <= 0 fdwor death (optional challenge)
 	print("Health changed by: ", amount)
+var coins = 0
 
-
+func change_coins(amount:int):
+	coins+=amount
+	print("you have"+ str (coins) + "coins")
 # TODO: Create shooting function
 func shoot():
 	# TODO: Create a new projectile instance
@@ -103,3 +113,9 @@ func shoot():
 	# print("Shot projectile facing: ", facing)
 	
 	pass
+
+
+
+
+func die():
+	print ("game over")
--- scripts/spike.gd
+++ scripts/spike.gd
@@ -1,11 +1,9 @@
 extends Area2D
 
 func _on_body_entered(body):
-	pass
-	# TODO: Check if the object that touched the spike is the player
-	# Use an if statement to check: if body.name == "Player":
-	
-	
+	if body.name == "Player":
+		body.change_health(-10)
+		("Player hit spikes!")
 	# TODO: Make the player take damage
 	# Call the player's health change function: body.change_health(-10)
 	
