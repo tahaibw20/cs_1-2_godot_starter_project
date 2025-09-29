99077d294d398b16c741af5b8afcbd90181651c7
tahaibw20 <talsaood1964@student.pps.net>
2025-09-29 09:59:50 -0700

    updated turret (barely)

--- scripts/turret.gd
+++ scripts/turret.gd
@@ -1,5 +1,7 @@
 extends Node2D
+var projectile_scene = preload("res://scenes/projectile.tscn")
 
+var player_in_range = false
 # TODO: Add variables for turret state
 # Track if player is in range, projectile scene, etc.
 # Look at "Boolean Variables and State Tracking" documentation
@@ -8,29 +10,45 @@ extends Node2D
 # Look at "Reusing Projectile Systems" documentation
 
 func _ready():
-	# TODO: Set up shooting timer
-	# Look at "Working with Timers" documentation
+	timer.wait_time = 2.0
+	timer.start()
 	pass
 
 func _on_detection_area_body_entered(body):
+	if body.name == "Player":
+		player_in_range = true
+		print("Player detected!")
+		
+		
 	# TODO: Check if the body is the player
 	# Update player detection state
 	# Look at "Boolean Variables and State Tracking" documentation
 	pass
 
+		
 func _on_detection_area_body_exited(body):
+	if body.name == "Player":
+		player_in_range = false
+		print("Player lost!")
 	# TODO: Check if the body is the player  
 	# Update player detection state
 	# Look at "Boolean Variables and State Tracking" documentation
 	pass
-
 func _on_shooting_timer_timeout():
+	if player_in_range:
+		print("Shooting at player!")
+	else:
+		print("No target in range")
 	# TODO: Check if player is in range
 	# If yes, shoot at player
 	# Look at documentation for shooting logic
 	pass
 
 func shoot_at_player():
+	new_projectile.global_position = global_position
+
+	new_projectile.set_direction_vector(calculated_direction)
+
 	# TODO: Get player's current position
 	# Calculate direction from turret to player
 	# Create and launch projectile
