613fd1a7a56cf48405741bfcee6f3397ff9e2807
tahaibw20 <talsaood1964@student.pps.net>
2025-11-03 09:57:15 -0800

    its a start

--- scenes/melee_minotaur.gd
+++ scenes/melee_minotaur.gd
@@ -1,8 +1,12 @@
 extends CharacterBody2D
 var maxHealth = 10
 var health = maxHealth
-
-
+var in_range = false
+var chasing = false
+var attacking = false
+#var projectile_original = preload("res://scenes/arrow.tscn")
+var speed = 200
+@onready var player: CharacterBody2D = $"."
 func change_health(_amount:int):
 		health += _amount
 		if health < 1:
@@ -13,3 +17,48 @@ func change_health(_amount:int):
 		
 func die():
 	queue_free()
+
+func _ready():
+	pass
+
+func _process(delta):
+	if in_range:
+		pass
+		
+	elif chasing and !attacking:
+		pass
+		
+	elif chasing and attacking:
+		pass
+	elif !in_range and !chasing and !attacking:
+		pass
+		
+func _on_melee_body_entered(body: Node2D) -> void:
+	if body.name == "Player":
+		attacking = true
+		
+
+func _on_melee_body_exited(body: Node2D) -> void:
+	if body.name == "Player":
+		attacking = false
+
+
+func _on_chase_body_entered(body: Node2D) -> void:
+	if body.name == "Player":
+		chasing = true
+		in_range = false
+
+
+func _on_chase_body_exited(body: Node2D) -> void:
+	if body.name == "Player":
+		chasing = false
+		in_range = true
+
+func _on_ranged_body_entered(body: Node2D) -> void:
+	if body. name == "Player":
+		in_range = true
+
+
+func _on_ranged_body_exited(body: Node2D) -> void:
+	if body. name == "Player":
+		in_range = false
